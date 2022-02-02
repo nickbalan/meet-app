2ae6db1e5f4639d656f257007227c6cfc2a5eb54
nickbalan <nickolae.balan@gmail.com>
2022-02-02 04:25:53 +0100

    G-Calendar API config update

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,18 +1,51 @@
-'use strict';
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+const calendar = google.calendar("v3");
+/*
+SCOPES allows you to set access levels; This is set to readonly.
+ */
+const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
 
-module.exports.hello = async (event) => {
+/*
+Credentials are those values required to get access to your calendar. 
+ */
+const credentials = {
+  client_id: process.env.CLIENT_ID,
+  project_id: process.env.PROJECT_ID,
+  client_secret: process.env.CLIENT_SECRET,
+  calendar_id: process.env.CALENDAR_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  redirect_uris: ["https://nickbalan.github.io/meet-app/"],
+  javascript_origins: ["https://nickbalan.github.io", "http://localhost:3000"],
+};
+const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
+const oAuth2Client = new google.auth.OAuth2(
+  client_id,
+  client_secret,
+  redirect_uris[0]
+);
+
+/*
+ The first step in the OAuth process is to generate a URL so users can log in with Google and be authorized to see your calendar. After logging in, they’ll receive a code as a URL parameter.
+ */
+module.exports.getAuthURL = async () => {
+  /*
+  Scopes array passed to the `scope` option. Any scopes passed must be enabled in the "OAuth consent screen" settings in your project on your Google Console. Also, any passed scopes are the ones users will see when the consent screen is displayed to them.
+   */
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({
+      authUrl: authUrl,
+    }),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
