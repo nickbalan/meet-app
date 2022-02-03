b5269cdcaa43c0851b6b44810fda202701dd53a0
nickbalan <nickolae.balan@gmail.com>
2022-02-03 04:11:16 +0100

    AWS Lambda config. Creating and testing a Serverless API

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -28,8 +28,8 @@ const oAuth2Client = new google.auth.OAuth2(
 );
 
 /*
- The first step in the OAuth process is to generate a URL so users can log in with Google and be authorized to see your calendar. After logging in, they’ll receive a code as a URL parameter.
- */
+The first step in the OAuth process is to generate a URL so users can log in with Google and be authorized to see your calendar. After logging in, they’ll receive a code as a URL parameter.
+*/
 module.exports.getAuthURL = async () => {
   /*
   Scopes array passed to the `scope` option. Any scopes passed must be enabled in the "OAuth consent screen" settings in your project on your Google Console. Also, any passed scopes are the ones users will see when the consent screen is displayed to them.
@@ -42,10 +42,115 @@ module.exports.getAuthURL = async () => {
   return {
     statusCode: 200,
     headers: {
-      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Origin": "*"
     },
     body: JSON.stringify({
       authUrl: authUrl,
     }),
   };
 };
+
+module.exports.getAccessToken = async (event) => {
+ /* 
+ The values used to instantiate the OAuthClient
+ */
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+
+  /* 
+  Decode authorization code extracted from the URL query
+  */
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+  return new Promise((resolve, reject) => {
+  /*
+  Exchange authorization code for access token with a “callback” after the exchange,
+  The callback in this case is an arrow function with the results as parameters: “err” and “token.”
+   */
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(token);
+    });
+  })
+  .then((token) => {
+    // Respond with OAuth token 
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify(token),
+    };
+  })
+  .catch((err) => {
+    // Handle error
+    console.error(err);
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify(err),
+    };
+  });
+};
+
+module.exports.getCalendarEvents = event => {
+  
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+
+  /* 
+  Decode authorization code extracted from the URL query
+  */
+  const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+  oAuth2Client.setCredentials({ access_token });
+  
+  return new Promise((resolve, reject) => { 
+
+    calendar.events.list(
+      {
+        calendarId: calendar_id,
+        auth: oAuth2Client,
+        timeMin: new Date().toISOString(),
+        singleEvents: true,
+        orderBy: "startTime",
+      },
+      (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
+      }
+    );
+  })
+  .then( results => {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify({ events: results.data.items }),
+    };
+  })
+  .catch((err) => {
+    // Handle error
+    console.error(err);
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify(err),
+    };
+  });
+}
