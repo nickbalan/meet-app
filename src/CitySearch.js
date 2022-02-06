f2f3dfe1bbc0f17b3dfd93cb46bd8e9a60c5b645
nickbalan <nickolae.balan@gmail.com>
2022-02-06 23:23:48 +0100

    Test Scenario 2

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
 
 class CitySearch extends Component {
-  render() {
-    return (
-        <div className='CitySearch'>
-            <input
-                type='text'
-                className='city'
-            />
-            <ul className='suggestions'>
-            </ul>
-        </div>
-    );
-  }
+    
+    constructor() {
+        super();
+     
+        this.state = {
+          query: '',
+          suggestions: []
+        }
+    }
+    
+    handleInputChanged = (event) => {
+        const value = event.target.value;
+        const suggestions = this.props.locations.filter((location) => {
+            return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+        });
+        this.setState({
+            query: value,
+            suggestions,
+        });
+    }; 
+
+    render() {
+        return (
+            <div className='CitySearch'>
+                <input
+                    type='text'
+                    className='city'
+                    value={this.state.query}
+                    onChange={this.handleInputChanged}
+                />
+                <ul className="suggestions">
+                    {this.state.suggestions.map((suggestion) => (
+                        <li key={suggestion}>{suggestion}</li>
+                    ))}
+                    <li key='all'>
+                        <b>See all cities</b>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default CitySearch;
